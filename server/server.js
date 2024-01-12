9961704fcff24976dd7141414180edf0ec4c87ba
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-12 12:25:39 -0500

    add todo endpoint created

--- server/server.js
+++ server/server.js
@@ -1,16 +1,35 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const pool = require("./db");
+const pool = require("./db"); //pool allows us to run queries with Postgres
 
 const app = express();
 
-require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 //middlewear
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //allows to parse to JSON data // req.body
+
+//create ROUTES
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+    try {
+      //console.log(req.body);
+      const { description } = req.body;
+      const newTodo = await pool.query(
+        "INSERT INTO todo (description) VALUES ($1)",
+        [description]
+      );
+      res.json(newTodo);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
