cde4e91091adc37fcb6e649313beabb15f724be1
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-12 12:25:15 -0500

    console logging values for debugging failed connection to db

--- server/db.js
+++ server/db.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
@@ -7,5 +9,14 @@ const pool = new Pool({
     port: process.env.PSQL_PORT,
     database: process.env.PSQL_DATABASE
 });
+
+//console to make sure the values are correct and to help debug
+
+console.log("Connecting to PostgreSQL with the following details:");
+console.log("User:", process.env.PSQL_USER);
+console.log("Password:", process.env.PSQL_PWD);
+console.log("Host:", process.env.PSQL_HOST);
+console.log("Port:", process.env.PSQL_PORT);
+console.log("Database:", process.env.PSQL_DATABASE);
 
 module.exports = pool;
