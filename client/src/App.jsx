de2db619f1f0c81e59e9db64d8afffb19f724449
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-14 12:57:06 -0500

    created ListTodos component with http GET all todos request and imported into the App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import InputTodo from './component/inputTodo';
 import "./App.css";
+import InputTodo from './component/inputTodo';
+import ListTodos from './component/ListTodos';
 
 function App() {
 
   return (
     <>
       <InputTodo />
+      <ListTodos />
     </>
   )
 }
--- client/src/component/ListTodos.jsx
+++ client/src/component/ListTodos.jsx
@@ -1,9 +1,47 @@
+import React, { useState, useEffect } from "react";
+
 const ListTodos = () => {
 
+    const [todos, setTodos] = useState([]);
+
+    const getAllTodos = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "GET",
+                headers: {"Content-Type": "application/json"},
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            };
+
+            const data = await response.json();
+            setTodos(data);
+            console.log(data);
+            
+        } catch (err) {
+            console.error(err.message);
+        }   
+    }
+
+    useEffect(() => {
+        getAllTodos();
+    }, []);
+
     return (
-
-        <></>
-
+        <>
+            <ul>
+                {todos.map((todo) => {
+                    return (
+                        <>
+                            <li key={todo.id}>{todo.description}</li>
+                            <button>edit</button>
+                            <button>delete</button>
+                        </>
+                    )
+                })};
+            </ul>
+        </>
     );
 
 }
