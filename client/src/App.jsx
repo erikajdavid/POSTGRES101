1de23283ee104c0c2b60cb90fdc587ebf52eb65c
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-14 12:41:58 -0500

    created todo input component with http POST request to server and imported into the app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,6 @@ function App() {
   return (
     <>
       <InputTodo />
-      <form>
-        <input type="text" className="form-control"></input>
-        <button type="submit" className='btn btn-success'>Add Todo</button>
-      </form>
     </>
   )
 }
--- client/src/component/InputTodo.jsx
+++ client/src/component/InputTodo.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputTodo = () => {
 
-    return (
-        <h1>Pern Todo List</h1>
-    );
+    const [description, setDescription] = useState("");
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const body = { description };
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body),
+            });
+
+            console.log(response);
+
+            //clean input
+
+            setDescription("");
+            
+        } catch (err) {
+            console.error(err.message);        }
+
+    }
+    return (
+        <>
+            <h1>Pern Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="todo"></label>
+                <input 
+                    type="text" 
+                    id="description" 
+                    name="description" 
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Enter a todo..." 
+                    required>
+                </input>
+                <button type="submit">+</button>
+            </form>
+        </>
+    );
 }
 
 export default InputTodo;
