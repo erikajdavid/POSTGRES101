fd18cae59cc3237015fc285c4ec18daf56edcb87
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-14 13:25:34 -0500

    http DELETED request on click completed

--- client/src/component/ListTodos.jsx
+++ client/src/component/ListTodos.jsx
@@ -4,6 +4,20 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    //delete todo
+    const deleteTodoOnClick = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE",
+            });
+            
+            console.log(deleteTodo);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    //get all todos
     const getAllTodos = async () => {
         try {
             const response = await fetch("http://localhost:5000/todos", {
@@ -37,7 +51,7 @@ const ListTodos = () => {
                             <li key={todo.todo_id}>
                                 {todo.description}
                                 <button>edit</button>
-                                <button>delete</button>
+                                <button onClick={() => deleteTodoOnClick(todo.todo_id)}>delete</button>
                             </li>
                         </>
                     )
