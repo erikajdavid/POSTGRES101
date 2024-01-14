b537708018397b75dd3b1052146ce0367263b015
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-13 21:02:29 -0500

    create update to do route

--- server/server.js
+++ server/server.js
@@ -21,11 +21,10 @@ app.post("/todos", async (req, res) => {
     try {
       //console.log(req.body);
       const { description } = req.body;
-      const newTodo = await pool.query(
-        "INSERT INTO todo (description) VALUES($1) RETURNING *",
-        [description]
-      );
+      const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+
       res.json(newTodo.rows[0]);
+
     } catch (err) {
       console.error(err.message);
     }
@@ -36,7 +35,9 @@ app.post("/todos", async (req, res) => {
 app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo");
+
     res.json(allTodos.rows)
+    
   } catch (error) {
     console.error(err.message);
   }
@@ -47,12 +48,28 @@ app.get("/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
     res.json(todo.rows[0]);
     
   } catch (error) {
     console.error(err.message);
   }
-})
+});
+
+//update a todo
+app.post("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *",
+    [description, id]);
+
+    res.json(updateTodo.rows[0]);
+
+  } catch (error) {
+    console.error(err.message);
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
