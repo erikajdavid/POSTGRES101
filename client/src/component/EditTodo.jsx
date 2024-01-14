a82808e2a6e38a43820ee76b43c430c38b101c5c
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-14 17:04:25 -0500

    created EditTodo component

--- client/src/component/EditTodo.jsx
+++ client/src/component/EditTodo.jsx
@@ -1,11 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
+
 const EditTodo = () => {
+    const [complete, setComplete] = useState(false);
+
+    const handleToggleComplete = () => {
+        setComplete(!complete);
+    };
 
     return (
-
-        <></>
-
+        <>
+            <div onClick={handleToggleComplete}>
+                {complete ? (
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                ) : (
+                    <FontAwesomeIcon icon={faCircle} />
+                )}
+            </div>
+        </>
     );
-
 }
 
 export default EditTodo;
